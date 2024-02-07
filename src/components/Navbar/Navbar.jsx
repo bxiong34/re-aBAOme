c27c1260865829382875c679c5b5d0f849005f60
bxiong34 <111828461+bxiong34@users.noreply.github.com>
2024-02-06 23:13:37 -0600

    added portfolio images and links

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ function Navbar () {
       <ul>
         <li className='name'>Bao Xiong</li>
         <li className='nav-item'><Link to="/" className={currentPage === '/' ? 'nav-link active' : 'nav-link'}>About Me</Link></li>
-        <li className='nnav-itemav'><Link to="/portfolio" className={currentPage === '/portfolio' ? 'nav-link active' : 'nav-link'}>Portfolio</Link></li>
+        <li className='nav-item'><Link to="/portfolio" className={currentPage === '/portfolio' ? 'nav-link active' : 'nav-link'}>Portfolio</Link></li>
         <li className='nav-item'><Link to="/contact" className={currentPage === '/contact' ? 'nav-link active' : 'nav-link'}>Contact</Link></li>
         <li className='nav-item'><Link to="/resume" className={currentPage === '/resume' ? 'nav-link active' : 'nav-link'}>Resume</Link></li>
       </ul>
--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -1,17 +1,73 @@
 import './Portfolio.css';
+import work1 from '../../assets/nami.png';
+import work2 from '../../assets/abaome-portfolio.png';
+import work3 from '../../assets/javascript-quiz.png';
+import work4 from '../../assets/daily-planner.png';
+import work5 from '../../assets/moodvie-generator.png';
+import work6 from '../../assets/weather-forecast.png';import work7 from '../../assets/expense-insight-tracker.png';
+import work8 from '../../assets/text-editor.png';
+
+const work = [
+  {
+    image: work1,
+    deployed: 'https://bxiong34.github.io/Nami/',
+    github: 'https://github.com/bxiong34/Nami.git',
+  },
+  {
+    image: work2,
+    deployed: 'https://bxiong34.github.io/aBAOme/',
+    github: 'https://github.com/bxiong34/aBAOme.git',
+  },
+  {
+    image: work3,
+    deployed: 'https://bxiong34.github.io/JavaScript-Quiz/',
+    github: 'https://github.com/bxiong34/JavaScript-Quiz.git',
+  },
+  {
+    image: work4,
+    deployed: 'https://bxiong34.github.io/daily-planner/',
+    github: 'https://github.com/bxiong34/daily-planner.git',
+  },
+  {
+    image: work5,
+    deployed: 'https://bxiong34.github.io/moodvie-generator/',
+    github: 'https://github.com/bxiong34/moodvie-generator.git',
+  },
+  {
+    image: work6,
+    deployed: 'https://bxiong34.github.io/weather-forecast/',
+    github: 'https://github.com/bxiong34/weather-forecast.git',
+  },
+  {
+    image: work7,
+    deployed: 'https://expense-insight-tracker-f044b665c1bb.herokuapp.com/login',
+    github: 'https://github.com/DavidJ92/Expense-Insight-Tracker.git',
+  },
+  {
+    image: work8,
+    deployed: 'https://text-editor-hh1w.onrender.com',
+    github: 'https://github.com/bxiong34/text-editor.git',
+  },
+]
 
 export default function Portfolio() {
   return (
     <div>
       <h1>Portfolio</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+      <div className='container'>
+        {work.map((item, index) => (
+          <div key={index} className='work-container'>
+           <div className='image-container'>
+              <img src={item.image} alt={`Work ${index}`} />
+              <div className='links'>
+                <a href={item.deployed} className='hide-link'>Deployed Site</a>
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <a href={item.github} className='hide-link'>Github URL</a>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
