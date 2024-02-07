de4d35b36a27dad8129d36636a8816cdb58a3e58
bxiong34 <111828461+bxiong34@users.noreply.github.com>
2024-02-06 18:39:05 -0600

    added components for navbar and different pages for nav titles

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Navbar.css';
+import { Link, useLocation } from 'react-router-dom';
+
+function Navbar () {
+  const currentPage = useLocation().pathname;
+
+  return (
+    <nav>
+      <ul>
+        <li className='name'>Bao Xiong</li>
+        <li className='nav-item'><Link to="/" className={currentPage === '/' ? 'nav-link active' : 'nav-link'}>About Me</Link></li>
+        <li className='nnav-itemav'><Link to="/portfolio" className={currentPage === '/portfolio' ? 'nav-link active' : 'nav-link'}>Portfolio</Link></li>
+        <li className='nav-item'><Link to="/contact" className={currentPage === '/contact' ? 'nav-link active' : 'nav-link'}>Contact</Link></li>
+        <li className='nav-item'><Link to="/resume" className={currentPage === '/resume' ? 'nav-link active' : 'nav-link'}>Resume</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import App from './App';
+import Error from './pages/Error';
+import About from './pages/About/About';
+import Portfolio from './pages/Portfolio/Portfolio';
+import Contact from './pages/Contact/Contact';
+import Resume from './pages/Resume/Resume';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <About />,
+      },
+      {
+        path: '/portfolio',
+        element: <Portfolio />,
+      },
+      {
+        path: '/contact',
+        element: <Contact />,
+      },
+      {
+        path: '/resume',
+        element: <Resume />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={router} />
+);
